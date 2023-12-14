00e427d8afc09689399c9a8c206fb1790ebaec30
emretkin <130176870+emretkin@users.noreply.github.com>
2023-12-14 20:29:03 +0300

    Add files via upload

---
+++ queueodev.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+struct node {
+  int data;
+  struct node* next;
+};
+static int boyut=0;
+struct node* front=NULL;
+struct node* temp=NULL;
+struct node* rear=NULL;
+
+void ekle(int id){
+	struct node* new=(struct node*)malloc(sizeof(struct node));
+
+	new->data=id;
+	new->next=NULL;
+	switch(id){
+		
+	case 1:
+	if(front==NULL){
+	front=new;
+	rear=new;
+    boyut++;
+    }
+	else{
+	rear->next=new;
+	rear=new;	
+	boyut++;
+	}
+    break;
+	
+    case 2:
+  	if(front==NULL){
+	front=new;
+	rear=new;
+    boyut++;
+	}
+	else if(boyut==1||boyut==2){
+	rear->next=new;
+	rear=new;	
+	boyut++;
+	}
+	else{
+		temp=front->next->next;
+		front->next->next=new;
+		new->next=temp;
+		boyut++;
+	}
+	break;
+	
+	case 3:
+	if(front==NULL){
+	front=new;
+	rear=new;
+    boyut++;
+    }
+  	else{
+		temp=front;
+		new->next=temp;
+		front=new;
+		boyut++;
+	}  
+	break;
+		
+    
+}	
+}
+
+void cikar(){
+	if (front == NULL) {
+        printf("Sýra boþ, iþlem yapacak kimse yok.\n");
+    }
+    else{
+    temp=front;
+    front=temp->next;
+    free(temp);
+    boyut--;
+    }
+}
+
+void yazdir(){
+	if (front == NULL) {
+    }
+    else{
+	temp=front;
+	while(temp->next!=NULL){
+		printf("ID%d=>",temp->data);
+		temp=temp->next;
+	}
+	printf("ID%d\n",temp->data);
+		}
+}
+
+void main(){
+	setlocale(LC_ALL, "Turkish");
+	int secim,id;
+	while(1){
+	printf("Hangi iþlem olsun ?\n\n");
+	printf("1.Kuyruða Girme Ýþlemi\n");
+	printf("2.Kuyruktakini Ýþleme Alma\n");
+	printf("3.Kuyruðu Görüntüleme Ýþlemi\n");
+	scanf("%d",&secim);
+	
+	switch(secim){
+	case 1:
+	      printf("ID Numaranýzý girin:");
+	      scanf("%d",&id);
+	      ekle(id);
+	      yazdir();
+	      break;
+	
+	case 2:
+		 cikar();
+		yazdir();
+	 
+		 break;
+		 
+	case 3:
+	     yazdir();
+		 break;
+		 	 
+	default:
+            printf("Hatalý bir seçim yapýldý.\n");
+	}
+	}
+		}
+	
